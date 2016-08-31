b73ad14a6fbe8e57d23945aec5f438818829f9bb
Terry Brown <terry_n_brown@yahoo.com>
2016-08-30 21:05:09 -0500

    make maps with QGIS

--- files/make.sh
+++ files/make.sh
@@ -3,4 +3,8 @@ for FMT in kml gpx; do
 done
 rm copts.geojson
 ogr2ogr -f GeoJSON copts.geojson copts.gpx waypoints
+qgis --snapshot co0Capture.png --width 1024 qgismap.qgs
+qgis --snapshot co1Capture.png --width 1024 \
+  --extent -11808392.66,4787802.11,-11799300.21,4793904.79 \
+  qgismap.qgs
 
